360d4173ee31118e75add706aa7c75ffc3d42b2c
Zachary Lansing <2424908+Zachjnl@users.noreply.github.com>
2021-09-26 22:03:31 -0500

    Update Supplier.java

--- Project 1/Supplier.java
+++ Project 1/Supplier.java
@@ -48,23 +48,4 @@ public class Supplier implements Serializable {
 		this.id = this.id + "12300" + sid;
 	}
 	
-	public static void main(String[] args){
-		Supplier test0 = new Supplier("Things & Stuff", "1234 Warehouse Road", "6892134765");
-		Supplier test1 = new Supplier("whoops", "9823 North Road", "8023476509");
-		Supplier test2 = new Supplier("Uuuhhhhh", "8735 East Road", "3934082567");
-		Supplier test3 = new Supplier("Food is good", "9834 South Road", "9238045364");
-		Supplier test4 = new Supplier("Whosimawhatsits", "3284 West Road", "8934052167");
-		Supplier test5 = new Supplier("Chicken Nuggets", "2837 Circle Road", "8934280576");
-		Supplier test6 = new Supplier("Party Time", "2139 Square Road", "9023428675");
-		SupplierList testList = new SupplierList();
-		testList.insertSupplier(test0);
-		testList.insertSupplier(test1);
-		testList.insertSupplier(test2);
-		testList.insertSupplier(test3);
-		testList.insertSupplier(test4);
-		testList.insertSupplier(test5);
-		testList.insertSupplier(test6);
-		System.out.println("This is an automated test of the Supplier and SupplierList classes\n" + testList.toString());
-	}
-	
 }

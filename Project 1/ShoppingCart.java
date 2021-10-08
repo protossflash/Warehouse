13f0fd60c0db72affc050b5b3c335e41592a236c
paullee247 <71791313+paullee247@users.noreply.github.com>
2021-10-07 20:15:23 -0500

    Update ShoppingCart.java

--- Project 1/ShoppingCart.java
+++ Project 1/ShoppingCart.java
@@ -1,52 +1,6 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
-
-public class ShoppingCart implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private List<ShoppingCartItem> cart;
-
-    public ShoppingCart() {
-        cart = new LinkedList<ShoppingCartItem>();
-    }
-
-    // insert a product into the shopping cart and the quantity
-    public boolean insertProductToCart(Product product, int quantity) {
-        ShoppingCartItem item = new ShoppingCartItem(product, quantity);
-        cart.add(item);
-        return true;
-    }
-
-    public Iterator<ShoppingCartItem> getShoppingCartProducts() {
-        return cart.iterator();
-    }
-
-    public double getTotalPrice() {
-        double totalPrice = 0;
-        Iterator<ShoppingCartItem> cartIterator = cart.iterator();
-
-        while (cartIterator.hasNext()){
-            ShoppingCartItem item = cartIterator.next();
-            totalPrice += (item.getProduct().getSalePrice() * item.getQuantity());
-        }
-
-        return totalPrice;
-    }
-
-    public String toString() {
-        return cart.toString();
-    }
-}
-
-
-
-
-
-
-
-
-    import java.util.*;
-        import java.lang.*;
-        import java.io.*;
 
 public class ShoppingCart implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -88,14 +42,4 @@ public class ShoppingCart implements Serializable {
     public String toString() {
         return cart.toString();
     }
-
-    public static void main(String[] args)
-    {
-        System.out.println("Testing ShoppingCart.java");
-        Product testp = new Product("1304", "Bestbuy Hammer", 20,10.00);
-        ProductList testpl = new ProductList();
-        testpl.insertProduct(testp);
-        System.out.println(testpl.toString());
-    }
-
 }
